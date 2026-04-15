941cd14780a806751f864a6cd395d9b9f65b9344
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2026-04-15 15:18:41 -0300

    .

---
+++ exemplo18.php
@@ -0,0 +1,79 @@
+<?php
+
+class Conta
+{
+    // Atributos - Propiedades - Campos
+    private $numero;
+    private $saldo;
+
+    //Método Construtor
+    function __construct($numero, $saldo)
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    
+    }
+    //MÉTODOS GETTERS E SETTERS
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function getSaldo(){
+        return $this->saldo;
+    }
+    protected function setSaldo($novoSaldo){
+        $this->saldo=$novoSaldo;
+    }
+
+    //Métodos
+    function creditar($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+
+    function debitar($valor)
+    {
+        $this->saldo = $this->saldo - $valor;
+    }
+
+    function transferir($outraConta, $valor) 
+    {
+        if($this->saldo >= $valor){
+           $this->debitar($valor);
+           $outraConta->creditar($valor);
+        }
+    }
+}
+
+class Poupanca extends Conta
+{
+    protected $juros;
+
+    function __construct($numero, $saldo, $juros)
+    {
+      parent::__construct($numero, $saldo);
+      $this->juros = $juros;
+    }
+
+    function creditar($valor)
+    {
+        parent::creditar($valor);
+        $this->atualizarJuros();
+    }
+
+    function atualizarJuros()
+    {
+        $this->saldo = $this->saldo * (1 + $this->juros);
+    }
+}
+
+$conta= new Conta(1, 150);
+$conta->creditar(50);
+$conta->debitar(100);
+echo "Saldo da Poupança {$conta->getNumero()}: R$ {$conta->getSaldo()} <br>";
+
+$poupanca = new Poupanca(2,150, 0.10);
+$poupanca->creditar(50);
+$poupanca->debitar(100);
+$poupanca->atualizarJuros();
+echo "Saldo da Poupança {$poupanca->getNumero()}: R$ {$poupanca->getSaldo()} <br>";
