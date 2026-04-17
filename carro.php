d6f60145d2cb2040f1bc5face847ec02f6b6d277
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2026-04-17 08:18:23 -0300

    funcionando(eu acho)

--- carro.php
+++ carro.php
@@ -19,9 +19,10 @@ class Carro {
 
         if($novaVelocidade >= 0 && $novaVelocidade <= 200){
             $this->velocidade= $novaVelocidade;
+            echo"Você está dentro da velocidade";
         }
         else{
-            echo"Está acima da velocidade permitida";
+            echo"Está acima da velocidade permitida<br>";
         }
     }
 }
@@ -30,8 +31,8 @@ class Carro {
 $meuCarro = new Carro("Senai-Mobile", 0);
 
 // O desastre: alteração direta sem validação
-$meuCarro->setVelocidade =(5000) ; // Velocidade de foguete?
-$meuCarro->setVelocidade = (-60);   // Carro andando no tempo?
+$meuCarro->getVelocidade(5000)  ; // Velocidade de foguete?
+$meuCarro->setVelocidade(-60);   // Carro andando no tempo?
 
 echo "Modelo: " . $meuCarro->modelo . "<br>";
 echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
