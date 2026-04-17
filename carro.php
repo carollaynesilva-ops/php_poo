920de328d62f477d1d2eaaf441b15f3a047ada7f
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2026-04-17 08:41:22 -0300

    desistencias e arrumações

--- carro.php
+++ carro.php
@@ -19,7 +19,7 @@ class Carro {
 
         if($novaVelocidade >= 0 && $novaVelocidade <= 200){
             $this->velocidade= $novaVelocidade;
-            echo"Você está dentro da velocidade";
+            echo"Você está dentro da velocidade<br>";
         }
         else{
             echo"Está acima da velocidade permitida<br>";
@@ -32,11 +32,11 @@ class Carro {
 // } desisti :(
 
 // --- TESTE DO VEÍCULO ---
-$meuCarro = new Carro("Senai-Mobile", 0);
+$meuCarro = new Carro("Senai-Mobile", 200);
 
 // O desastre: alteração direta sem validação
-$meuCarro->setVelocidade(5000)  ; // Velocidade de foguete?
-$meuCarro->getVelocidade(-60);   // Carro andando no tempo?
+$meuCarro->setVelocidade(200)  ; // Velocidade de foguete?
+$meuCarro->getVelocidade();   // Carro andando no tempo?
 
 echo "Modelo: " . $meuCarro->modelo . "<br>";
 echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
